b6993888ea91cd895db909536ef956f1042a0444
garylucasgaming <paradoxsc2@gmail.com>
2020-06-11 06:43:39 -0400

    gary 6/11/2020
    background scroll
    ui manager/all ui
    phone ui set up
    scene  manager

---
+++ Assets/Scripts/JoyButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+
+    protected bool Pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+
+
+    private void Update()
+    {
+        print(Pressed);
+    }
+}
