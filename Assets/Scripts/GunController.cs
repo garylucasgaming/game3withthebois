d525b4affdcd28e6c27b975cfeed6616022d0e9a
Kris J <KrisJohnson458@gmail.com>
2020-06-14 05:59:56 -0500

    Need to be Up to Date
    updating my branch

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+
+    public Gun gunScriptable;
+    public AudioSource gunSound;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(gunScriptable != null)
+        {
+            gunSound.volume = gunScriptable.sound;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+            gunSound.Play();
+        }
+    }
+
+
+
+
+}
