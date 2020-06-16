e7c05d3da7f668b1efc7944161ccb9b7d47077f6
Vincent Manno <vincent.s.manno@gmail.com>
2020-06-16 05:09:04 -0400

    Revert "incomplete gun shooting, code for setting up gun"
    This reverts commit 3b37080c651020e5d96954dd67cba3e26a887a3f.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 
 [CreateAssetMenu(menuName =  "Gun")]
-class Gun : ScriptableObject
+    class Gun : ScriptableObject
 {
-    public Sprite projectileObject;
-    public int sound;
-    public int numberOfSpawnPoints;
-    public int rateOfFire;
+    private Sprite projectileObject;
+    private int sound;
+    private int numberOfSpawnPoints;
+    private int rateOfFire;
 }
 
