c553236d1e9875ade8821437d1d57f3a1a10874d
Vincent Manno <vincent.s.manno@gmail.com>
2020-06-16 04:48:50 -0400

    Revert "Merge branch 'master' into Vincent"
    This reverts commit 385fa182614432557988148d43548f4dd0e83c30, reversing
    changes made to 3b37080c651020e5d96954dd67cba3e26a887a3f.

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,65 +4,16 @@ using UnityEngine;
 
 
 
-class Asteroid : MonoBehaviour, ICanPath
+class Asteroid : ICanPath
 {
-    [SerializeField]private int damage;
-    [SerializeField]private int speed;
+    private int damage;
+    private int speed;
     private int sprite;
-    [SerializeField] public int lives;
-    private int waypointIndex = 0;
-    [SerializeField] public List<Transform> path;
-
-    private void Start()
-    {
-        transform.position  = new Vector3(path[waypointIndex].position.x, path[waypointIndex].position.y, 0);
-    }
-
-    private void Update()
-    {
-       MoveAlongPath();
-        transform.Rotate(0, 0, 180 * Time.deltaTime);
-    }
+    private int health;
 
     public void MoveAlongPath()
     {
-        if (waypointIndex <= path.Count - 1)
-        {
-            var targetPosition = new Vector3(path[waypointIndex].position.x, path[waypointIndex].position.y, 0);
-
-
-            transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
-
-            if (transform.position == targetPosition)
-            {
-                waypointIndex++;
-            }
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        
     }
-
-    public void SetPath(List<Transform> newPath) {
-        path = newPath;
-    }
-
-    public void OnDeath()
-    {
-        Destroy(gameObject);
-    }
-
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            collision.gameObject.GetComponent<ShipModel>().Lives--;
-            OnDeath();
-
-        }
-    }
-
 }
 
